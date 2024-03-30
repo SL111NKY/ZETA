52f8cdc13d0ebc5e2178141bb354ed2636402ba4
SL111NKY <132756455+SL111NKY@users.noreply.github.com>
2024-03-30 02:42:54 +0100

    adding more php files

---
+++ php/utils/loader.php
@@ -0,0 +1,12 @@
+<div id="loading">
+	<!--<img id="loading-image" src="path/to/ajax-loader.gif" alt="Loading..." />-->
+	<div id="loader">
+		<div class="loader-square"></div>
+		<div class="loader-square"></div>
+		<div class="loader-square"></div>
+		<div class="loader-square"></div>
+		<div class="loader-square"></div>
+		<div class="loader-square"></div>
+		<div class="loader-square"></div>
+	</div>
+</div>
---
+++ php/utils/nav.php
@@ -0,0 +1,13 @@
+<nav>
+	<div class="navwrapper">
+		<div class="logo">
+			<a href="home.php"><img src="../images/logo/logo.png" id="navlogo" class="hithere" style="width: 5%;" />
+				</a>
+		</div>
+		<ul class="nav-links">
+			<li><a href="profile.php">ACCOUNT</a></li>
+			<li><a href="ranking.php">RANKING</a></li>
+			<li><a href="logout.php">LOGOUT</a></li>
+		</ul>
+	</div>
+</nav>
